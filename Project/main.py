12a3159d8ae0ba51dce8b8183ad02668aaed3f15
Snuwiu <m_urosevic@outlook.com>
2023-11-17 16:07:08 +0100

    Tried getting text to speech to work

--- Project/main.py
+++ Project/main.py
@@ -1,72 +1,45 @@
-# Imports
-from __future__ import annotations
 import openai
+import time
 import creds
-import view
-from multiprocessing import *
 
-# API Key
 openai.api_key = creds.apikey
 
+client = openai.OpenAI()
 
-def get_api_response(prompt: str) -> str | None:
-    text: str | None = None
+assistant = client.beta.assistants.create(
+    name = "Snowflake",
+    instructions = "You are my friend snowflake. You are an AI that helps me with my everyday tasks",
+    tools = [{"type":"code_interpreter"}],
+    model = "gpt-4-1106-preview"
+)
 
-    try:
-        response: dict = openai.Completion.create(
-            model='text-davinci-003',
-            prompt=prompt,
-            temperature=0.9,
-            max_tokens=150,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0.6,
-            stop=[' Human:', ' AI:']
-        )
+thread = client.beta.thread.create()
 
-        choices: dict = response.get('choices')[0]
-        text = choices.get('text')
+message = client.beta.threads.message.create(
+    thread_id = thread.id,
+    role = "Snuwiu",
+    content = "Hi Snowflake, how are you?"
+)
 
-    except Exception as e:
-        print('ERROR:', e)
+run = client.beta.threads.runs.create(
+    thread_id = thread.id,
+    assistant_id = assistant.id,
+    instructions = "The person you talk to is called Snuwiu but you call him Sneaver"
+)
 
-    return text
+time.sleep(10)
 
+run_status = client.beta.threads.runs.retrueve(
+    thread_id = thread.id,
+    assistant_id = assistant.id
+)
 
-def update_list(message: str, pl: list[str]):
-    pl.append(message)
+if run_status == 'completed':
+    messages = client.beta.threads.messages.list(
+        thread_id = thread.id
+    )
 
-
-def create_prompt(message: str, pl: list[str]) -> str:
-    p_message: str = f'\nHuman: {message}'
-    update_list(p_message, pl)
-    prompt: str = ''.join(pl)
-    return prompt
-
-
-def get_bot_response(message: str, pl: list[str]) -> str:
-    prompt: str = create_prompt(message, pl)
-    bot_response: str = get_api_response(prompt)
-
-    if bot_response:
-        update_list(bot_response, pl)
-        pos: int = bot_response.find('\nAI: ')
-        bot_response = bot_response[pos + 5:]
-    else:
-        bot_response = 'Something went wrong...'
-
-    return bot_response
-
-
-def main(user_input):
-    prompt_list: list[str] = ['You are an assistant and will answer as snowflake',
-                              '\nHuman: What time is it?',
-                              '\nAI: I have no idea, I\'m a snowflake!']
-
-    while True:
-        if user_input == "Stop program":
-            print("Snowflake: Sure")
-            exit()
-
-        response = get_bot_response(user_input, prompt_list)
-        print(f'Bot: {response}')
+    for msg in messages.data:
+        role = msg.role
+        content = msg.content[0].text.value
+        print(f"{role.compitalize()}: {content}")
